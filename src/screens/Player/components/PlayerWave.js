b7e2358b8ce24dc56bf846660268114cffc8f4b1
Muneeb <mymac@muneebs-air.home>
2022-01-16 11:07:03 +0500

    Listening Page

--- src/screens/Player/components/PlayerWave.js
+++ src/screens/Player/components/PlayerWave.js
@@ -2,20 +2,30 @@ import React from 'react';
 import {RFValue} from 'react-native-responsive-fontsize';
 import {widthPercentageToDP as wp} from 'react-native-responsive-screen';
 
-import {Row, Icon} from 'components';
+import {Row, Icon, MyText} from 'components';
 
-const PlayerOptions = () => {
+const PlayerWave = () => {
   return (
     <Row between center marg={`${wp(8)}px 0`}>
-      <Icon type="header-search" size={wp(6)} />
+      <MyText
+        marg={`0 0 ${wp(2)}px  0`}
+        weight="bold"
+        size={`${RFValue(12)}px`}>
+        3:20
+      </MyText>
       <Row noFlex center>
         <Icon type="header-search" size={wp(6)} />
         <Icon type="header-search" size={wp(6)} />
         <Icon type="header-search" size={wp(6)} />
       </Row>
-      <Icon type="header-search" size={wp(6)} />
+      <MyText
+        marg={`0 0 ${wp(2)}px  0`}
+        weight="bold"
+        size={`${RFValue(12)}px`}>
+        4:43
+      </MyText>
     </Row>
   );
 };
 
-export default PlayerOptions;
+export default PlayerWave;
--- src/screens/Player/index.js
+++ src/screens/Player/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {widthPercentageToDP as wp} from 'react-native-responsive-screen';
 
 import {PlayerHeader, Container} from 'components';
-import {Cover, SongDetails} from './components';
+import {Cover, SongDetails, PlayerOptions, PlayerWave} from './components';
 
 const index = ({navigation}) => {
   return (
@@ -13,6 +13,8 @@ const index = ({navigation}) => {
         pad={`${wp(12)}px ${wp(5)}px ${wp(25)}px ${wp(5)}px`}>
         <Cover />
         <SongDetails />
+        <PlayerOptions />
+        <PlayerWave />
       </Container>
     </>
   );
